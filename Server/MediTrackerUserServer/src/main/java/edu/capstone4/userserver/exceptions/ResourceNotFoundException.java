bd424aa8e62176206ba9044988197087c9076d04
yanran-cao <y.rikki.cao@gmail.com>
2024-11-02 19:59:12 -0700

    Add ResourceNotFoundException for handling user not found scenarios

--- Server/MediTrackerUserServer/src/main/java/edu/capstone4/userserver/exceptions/ResourceNotFoundException.java
+++ Server/MediTrackerUserServer/src/main/java/edu/capstone4/userserver/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package edu.capstone4.userserver.exceptions;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
