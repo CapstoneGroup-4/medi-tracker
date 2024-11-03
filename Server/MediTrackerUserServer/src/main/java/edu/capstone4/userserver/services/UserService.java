ed9bed67da30187ca4a687dafe545cd4a11f37e7
yanran-cao <y.rikki.cao@gmail.com>
2024-11-02 19:59:02 -0700

    Implement UserService to handle business logic for user management

--- Server/MediTrackerUserServer/src/main/java/edu/capstone4/userserver/services/UserService.java
+++ Server/MediTrackerUserServer/src/main/java/edu/capstone4/userserver/services/UserService.java
@@ -1,4 +1,56 @@
 package edu.capstone4.userserver.services;
+import edu.capstone4.userserver.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import edu.capstone4.userserver.repository.UserRepository;
+import edu.capstone4.userserver.exceptions.ResourceNotFoundException;
 
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User createUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User updateUser(Long id, User user) {
+        User existingUser = getUserById(id);
+        existingUser.setUsername(user.getUsername());
+        existingUser.setEmail(user.getEmail());
+        if (user.getPassword() != null) {
+            existingUser.setPassword(passwordEncoder.encode(user.getPassword()));
+        }
+        return userRepository.save(existingUser);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public Boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    public Boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
 }
