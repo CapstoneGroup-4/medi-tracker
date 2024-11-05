43c93f53af697960dda71260de606f443f2b987b
Ian <ianzhang000@hotmail.com>
2024-11-04 22:59:08 -0800

    Feature: init medical info chaincode

---
+++ Chaincode/src/main/java/edu/capstone4/chaincode/MedicalInfo.java
@@ -0,0 +1,90 @@
+package edu.capstone4.chaincode;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import org.hyperledger.fabric.contract.annotation.DataType;
+import org.hyperledger.fabric.contract.annotation.Property;
+
+import com.owlike.genson.annotation.JsonProperty;
+
+@DataType()
+public class MedicalInfo {
+
+    @Property()
+    private final String patientId;
+
+    @Property()
+    private final String patientName;
+
+    @Property()
+    private final String patientGender;
+
+    @Property()
+    private final String patientEmail;
+
+    @Property()
+    private final MedicalRecord[] medicalRecords; // Assuming MedicalRecord is correctly defined somewhere
+
+    public MedicalInfo(@JsonProperty("patientId") String patientId,
+                       @JsonProperty("patientName") String patientName,
+                       @JsonProperty("patientGender") String patientGender,
+                       @JsonProperty("patientEmail") String patientEmail,
+                       @JsonProperty("medicalRecords") MedicalRecord[] medicalRecords) {
+        this.patientId = patientId;
+        this.patientName = patientName;
+        this.patientGender = patientGender;
+        this.patientEmail = patientEmail;
+        this.medicalRecords = medicalRecords;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public String getPatientGender() {
+        return patientGender;
+    }
+
+    public String getPatientEmail() {
+        return patientEmail;
+    }
+
+    public MedicalRecord[] getMedicalRecords() {
+        return medicalRecords;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MedicalInfo that = (MedicalInfo) o;
+        return Objects.equals(patientId, that.patientId) &&
+                Objects.equals(patientName, that.patientName) &&
+                Objects.equals(patientGender, that.patientGender) &&
+                Objects.equals(patientEmail, that.patientEmail) &&
+                Arrays.equals(medicalRecords, that.medicalRecords);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(patientId, patientName, patientGender, patientEmail);
+        result = 31 * result + Arrays.hashCode(medicalRecords);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "MedicalInfo{" +
+                "patientId='" + patientId + '\'' +
+                ", patientName='" + patientName + '\'' +
+                ", patientGender='" + patientGender + '\'' +
+                ", patientEmail='" + patientEmail + '\'' +
+                ", medicalRecords=" + Arrays.toString(medicalRecords) +
+                '}';
+    }
+}
